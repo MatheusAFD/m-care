ed74722d4a02a953fb7bc46c6cf0fa06750b1462
matheusafd <matheus.daf@outlook.com>
2025-03-09 00:11:43 -0300

    refactor: update layout structure and remove unnecessary elements

--- src/features/@shared/components/app-sidebar/custom-sidebar/index.tsx
+++ src/features/@shared/components/app-sidebar/custom-sidebar/index.tsx
@@ -14,7 +14,7 @@ import {
 } from '../'
 import { Suspense } from 'react'
 
-export async function CustomSidebar() {
+export function CustomSidebar() {
   return (
     <Sidebar>
       <SidebarContent>
