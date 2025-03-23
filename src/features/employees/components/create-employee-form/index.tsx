d792543d47fb9361ddf677c58bddb05fff5a5d84
matheusafd <matheus.daf@outlook.com>
2025-03-23 19:46:07 -0300

    feat(employees): implement multi-step employee form

--- src/features/employees/components/create-employee-form/index.tsx
+++ src/features/employees/components/create-employee-form/index.tsx
@@ -1,19 +1,34 @@
-import {
-  PasswordField,
-  SwitchField,
-  TextField
-} from '@m-care/features/@shared/components'
+'use client'
+import { FormProvider, useForm } from 'react-hook-form'
+
+import { EmployeeFormStepEnum } from '../../enums'
+import { useEmployeeForm } from '../../hooks'
+import { EmployeeFormSchemaType, EmployeeFormSchema } from '../../types'
+import { EmployeePersonalDataStep } from './employee-personal-data-step'
+import { EmployeeAddressStep } from './employee-address-step'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export const CreateEmployeeForm = () => {
-  return (
-    <form className="w-full grid gap-4 grid-cols-2">
-      <div className="col-span-2">
-        <TextField id="name" label="Nome" className="col-span-2" />
-      </div>
-      <TextField id="email" label="E-mail" />
-      <PasswordField id="password" label="Senha" />
+  const { formStep } = useEmployeeForm()
 
-      <SwitchField id="status" label="Ativo" />
+  const formMethods = useForm<EmployeeFormSchemaType>({
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(EmployeeFormSchema)
+  })
+
+  const componentbyStep = {
+    [EmployeeFormStepEnum.PersonalData]: EmployeePersonalDataStep,
+    [EmployeeFormStepEnum.Address]: EmployeeAddressStep
+  }
+
+  const CurrentStepComponent = componentbyStep[formStep]
+
+  return (
+    <form>
+      <FormProvider {...formMethods}>
+        <CurrentStepComponent />
+      </FormProvider>
     </form>
   )
 }
