a6300dfd30ebe819f5c19fa7aebba929a6d9985b
matheusafd <matheus.daf@outlook.com>
2025-03-22 21:49:52 -0300

    feat(switch-field): add SwitchField component

---
+++ src/features/@shared/components/switch-field/index.tsx
@@ -0,0 +1,16 @@
+import { Switch } from '../ui'
+import { SwitchFieldProps } from './types'
+
+export const SwitchField = (props: SwitchFieldProps) => {
+  const { label, isRequired, id } = props
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Switch {...props} />
+
+      <label className="text-black text-xs" htmlFor={id}>
+        {label}: {isRequired && '*'}
+      </label>
+    </div>
+  )
+}
