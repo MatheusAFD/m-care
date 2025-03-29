eeb94adb45890c070ef0e872a6c9ddb2b9b17b09
matheusafd <matheus.daf@outlook.com>
2025-03-29 13:26:25 -0300

    refactor(employee-form-schema): update form schema

--- src/features/employees/types/employee-form-schema/index.ts
+++ src/features/employees/types/employee-form-schema/index.ts
@@ -1,20 +1,24 @@
 import { z } from 'zod'
 
 export const EmployeeFormSchema = z.object({
-  name: z.string().nonempty('Nome é obrigatório'),
+  name: z
+    .string()
+    .nonempty('Nome é obrigatório')
+    .refine(
+      (value) => value.trim().split(' ').length > 1,
+      'Informe o nome completo'
+    ),
   phone: z
     .string()
     .nonempty('O telefone é obrigatório')
     .min(11, 'O telefone deve ter pelo menos 11 caracteres'),
   email: z.string().email('Endereço de e-mail inválido'),
-  password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
   address: z.string().nonempty('Endereço é obrigatório'),
   city: z.string().nonempty('Cidade é obrigatória'),
   state: z.string().nonempty('Estado é obrigatório'),
   number: z.string().nonempty('Número é obrigatório'),
   neighborhood: z.string().nonempty('Bairro é obrigatório'),
   zipcode: z.string().nonempty('O CEP é obrigatório').min(8, 'CEP inválido'),
-  status: z.boolean(),
   isWhatsapp: z.boolean()
 })
 
