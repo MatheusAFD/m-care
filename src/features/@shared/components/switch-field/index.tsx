6bf643b643648138fc04cf8bad9caee3f5e593b3
matheusafd <matheus.daf@outlook.com>
2025-03-28 21:26:50 -0300

    feat(switch-field): integrate react-hook-form for controlled input

--- src/features/@shared/components/switch-field/index.tsx
+++ src/features/@shared/components/switch-field/index.tsx
@@ -1,16 +1,57 @@
+'use client'
+
+import { useRef } from 'react'
 import { Switch } from '../ui'
 import { SwitchFieldProps } from './types'
+import { Controller, Control } from 'react-hook-form'
 
-export const SwitchField = (props: SwitchFieldProps) => {
-  const { label, isRequired, id } = props
+interface Props extends SwitchFieldProps {
+  name: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<any>
+}
+
+export const SwitchField = ({
+  label,
+  isRequired,
+  id,
+  name,
+  control,
+  ...rest
+}: Props) => {
+  const ref = useRef<HTMLButtonElement | null>(null)
+
+  const handleClick = () => {
+    ref?.current?.click()
+  }
 
   return (
-    <div className="flex items-center space-x-2 font-medium">
-      <Switch {...props} />
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <div
+          className="flex items-center justify-between space-x-2 font-medium w-full h-12 max-h-12 px-3 py-[10px] rounded-md border border-grey text-sm cursor-pointer"
+          onClick={handleClick}
+        >
+          <label
+            className="text-black text-xs"
+            htmlFor={id}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {label} {isRequired && '*'}
+          </label>
 
-      <label className="text-black text-xs" htmlFor={id}>
-        {label}: {isRequired && '*'}
-      </label>
-    </div>
+          <Switch
+            {...rest}
+            id={id}
+            ref={ref}
+            checked={field.value}
+            onCheckedChange={field.onChange}
+            onClick={(e) => e.stopPropagation()}
+          />
+        </div>
+      )}
+    />
   )
 }
