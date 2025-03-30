c4bd47dfbab10ac621b5694c711d88d7c5a18a48
matheusafd <matheus.daf@outlook.com>
2025-03-30 17:13:09 -0300

    feat(employee-card): enhance UI with tooltip and edit button

--- src/features/employees/components/employee-card/index.tsx
+++ src/features/employees/components/employee-card/index.tsx
@@ -1,20 +1,57 @@
-import { Avatar } from '@m-care/features/@shared/components/ui'
-import { EmployeeCardProps } from './types'
-import { AvatarFallback } from '@radix-ui/react-avatar'
-import { getNameInitials } from '@m-care/features/@shared/utils'
+import { Edit } from 'lucide-react'
+
+import {
+  Avatar,
+  AvatarFallback,
+  Button,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger
+} from '@m-care/features/@shared/components/ui'
 import { CustomCard } from '@m-care/features/@shared/components'
 
+import { getNameInitials } from '@m-care/features/@shared/utils'
+import { EmployeeCardProps } from './types'
+
 export const EmployeeCard = (props: EmployeeCardProps) => {
-  const { name } = props
+  const { name, color } = props
 
   const nameInitials = getNameInitials(name)
 
   return (
-    <CustomCard>
-      <Avatar>
-        <AvatarFallback>{nameInitials}</AvatarFallback>
+    <CustomCard
+      className="relative p-0 w-72 h-40 flex flex-col items-center justify-center overflow-hidden"
+      {...props}
+    >
+      <div
+        className="absolute top-0 h-2 w-full"
+        style={{ backgroundColor: color }}
+      />
+
+      <Avatar className="size-16 border-2 " style={{ borderColor: color }}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <AvatarFallback>{nameInitials}</AvatarFallback>
+          </TooltipTrigger>
+          <TooltipContent>{name}</TooltipContent>
+        </Tooltip>
       </Avatar>
-      <p className="text-sm font-medium">{name}</p>
+      <div className="mt-4 py-1 px-4 rounded-lg border max-w-56">
+        <p className="text-xs text-center text-black font-medium ">{name}</p>
+      </div>
+
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className="absolute top-4 right-2"
+            variant="outline"
+            size="icon"
+          >
+            <Edit />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Editar colaborador</TooltipContent>
+      </Tooltip>
     </CustomCard>
   )
 }
