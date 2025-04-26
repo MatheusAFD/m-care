ddef62053783aeee414d8eabead16bbc50c99a1a
matheusafd <matheus.daf@outlook.com>
2025-04-26 13:38:11 -0300

    refactor(unit-card): improve layout and structure

--- src/features/units/components/unit-card/index.tsx
+++ src/features/units/components/unit-card/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { Edit, Map, MapPin } from 'lucide-react'
 
 import {
@@ -10,7 +12,6 @@ import {
 import { CustomCard } from '@m-care/features/@shared/components'
 
 import { UnitCardProps } from './types'
-import Image from 'next/image'
 
 export const UnitCard = ({ unit, onEdit, ...props }: UnitCardProps) => {
   const { name, address, neighborhood, state, city, number } = unit
@@ -29,27 +30,26 @@ export const UnitCard = ({ unit, onEdit, ...props }: UnitCardProps) => {
         />
       </Avatar>
 
-      <section className="">
-        <div className="mt-4 py-1 px-4 rounded-lg border max-w-56">
-          <p className="text-xs text-center text-black font-medium ">
-            {name} - {neighborhood}
-          </p>
+      <section>
+        <div className="mt-4 py-1 px-4 rounded-lg border max-w-56 min-h-10">
+          <p className="text-xs text-center text-black font-medium ">{name}</p>
+          <p className="text-xs text-center font-semibold">{neighborhood}</p>
         </div>
 
         <hr className="w-full h-[1px] left-0 bg-gray-200 mt-2 absolute" />
-
-        <div className="flex flex-col max-h-full mt-2">
-          <div className="flex items-center gap-1 mt-2">
-            <MapPin size={16} className="text-green-principal" />
-            <p className="text-xs font-medium">{`${city}/${state}`}</p>
-          </div>
-
-          <div className="flex items-center gap-1 mt-2">
-            <Map size={16} className="text-green-principal" />
-            <p className="text-xs font-medium">{`${address} - ${number}`}</p>
-          </div>
-        </div>
       </section>
+
+      <div className="flex flex-col justify-center h-full">
+        <div className="flex items-center gap-1 mt-2">
+          <MapPin size={16} className="text-green-principal" />
+          <p className="text-xs font-medium">{`${city}/${state}`}</p>
+        </div>
+
+        <div className="flex items-center gap-1 mt-2">
+          <Map size={16} className="text-green-principal" />
+          <p className="text-xs font-medium">{`${address} - ${number}`}</p>
+        </div>
+      </div>
 
       <Tooltip>
         <TooltipTrigger asChild>
