45a9ae10c01a6bb3998080c32b5532ecc096aeca
matheusafd <matheus.daf@outlook.com>
2025-04-28 19:48:09 -0300

    fix(toggle): add missing filter variant and size

--- src/features/@shared/components/ui/toggle.tsx
+++ src/features/@shared/components/ui/toggle.tsx
@@ -13,10 +13,13 @@ const toggleVariants = cva(
       variant: {
         default: 'bg-transparent ',
         outline:
-          'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground'
+          'border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
+        filter:
+          'text-xs border border-input rounded-full bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground'
       },
       size: {
         default: 'h-9 px-2 min-w-9',
+        filter: 'h-8 px-4 min-w-8',
         sm: 'h-8 px-1.5 min-w-8',
         lg: 'h-10 px-2.5 min-w-10'
       }
